1e544eb2fc6822ab6f33a34a6b56a0825b9e7764
Emerson <Emerson.Huang@hwgroup.com.tw>
2025-11-25 10:53:14 +0800

    Initial Commit

---
+++ hw_group_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw_group_app
+//
+//  Created by hwgroup on 2025/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hw_group_appTests/hw_group_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  hw_group_appTests.swift
+//  hw_group_appTests
+//
+//  Created by hwgroup on 2025/11/24.
+//
+
+import Testing
+@testable import hw_group_app
+
+struct hw_group_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
